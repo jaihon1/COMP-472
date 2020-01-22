98496e91bf3cd09d9821f1d0eccdb2583422c543
Jaihon Qasim <dzhay@Jaihons-MacBook-Pro.local>
2020-01-22 17:30:23 -0500

    Setting up project.

---
+++ src/main.py
@@ -0,0 +1,11 @@
+import module1
+
+
+
+def main():
+    print("python main function")
+
+
+if __name__ == '__main__':
+    main()
+    module1.main()
---
+++ src/module1.py
@@ -0,0 +1,9 @@
+
+
+def main():
+    print("This is an imported module")
+
+
+if __name__ == '__main__':
+    main()
+
