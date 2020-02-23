0c043919b9a84cc6f5caaa3fcd0efa1a119807c4
mathiaco <mathiaco2@gmail.com>
2020-02-22 22:40:54 -0500

    #3 update driver for new search algorithms

--- src/driver.py
+++ src/driver.py
@@ -1,17 +1,22 @@
 from modules.dfsearch import DFSearch
+from modules.bfsearch import BFSearch
+from modules.aStarsearch import AStarSearch
 from modules.board import Board
 
 import numpy as np
 
 def main():
 
+    ## Get file name
+    filename = input('What file name are the puzzles in ?')
+
     ## Reading input file ##
-    with open('test.txt') as f:
+    with open(filename) as f:
         puzzles = f.readlines()
 
-    ## Running DFS on them ##
+    ## Setup ##
     for puzzle in puzzles:
-        # Game Setup
+        # Boards Setup
         puzzleIndex = puzzles.index(puzzle)
         print("Puzzle #: ", puzzleIndex)
         print(puzzle)
@@ -19,6 +24,7 @@ def main():
         elements = puzzle.split()
         board_size = int(elements[0])
         max_depth = int(elements[1])
+        max_search_length = int(elements[2])
         boardValues = list(elements[3])
 
         counter = 0
@@ -36,13 +42,25 @@ def main():
         myBoard.initializeBoardOnes()
         myBoard.initializeBoardZeros()
 
-        # DFS setup
-        dfs = DFSearch(max_depth, puzzleIndex)
+        # Get search algorithm from user
+        algo = input('What algorithm do you wish to use ?')
+
+        if (algo=='DFS'):
+            # DFS setup
+            game = DFSearch(max_depth, puzzleIndex)
+        elif (algo == 'BFS'):
+            # BFS setup
+            game = BFSearch(max_search_length)
+        elif (algo == 'A*'):
+            # A* setup
+            game = AStarSearch(max_search_length)
+        else:
+            print('Algorithm not supported')
 
         print("Initial Board")
         print(myBoard.getBoard())
 
-        dfs.run(myBoard)
+        game.run(myBoard)
 
 
 
